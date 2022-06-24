c91bb8afd0c9f0b302d3fe45f88ce4593794f838
Brendan Willis <vuulgen2@gmail.com>
2022-06-24 17:45:35 -0600

    Trying to fix an error message

--- stedi-react-native-main/App.js
+++ stedi-react-native-main/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useState} from 'react';
-import { StyleSheet, View,  } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Counter from './Counter.js';
-import Login from "./Login.js";
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
+import Login from './Login.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
@@ -13,63 +12,60 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 
-export default function App(props) {
+export default function App() {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
-
-  if(userLoggedIn){
-
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-      
-        initialRouteName='Home'
-        activeColor='white'
-        barStyle={{ backgroundColor: 'green' }}
-      >
-        <Tab.Screen
-          name='Home'
-          component={Home}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Step Counter'
-          component={Counter}
-          options={{
-            tabBarLabel: 'Step Counter',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Settings'
-          component={SettingsScreen}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name='gear' color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-
-
-} else {
-  return (
-    <view>
-        <Login />
-    </view>
-  )
+        if (userLoggedIn) {
+   return (
+     <NavigationContainer>
+       <Tab.Navigator
+         initialRouteName='Home'
+         activeColor='wwhite'
+         barStyle={{ backgroundColor: 'green' }}
+       >
+         <Tab.Screen
+           name='Home'
+           component={Home}
+           options={{
+             tabBarLabel: 'Home',
+             tabBarIcon: ({ color }) => (
+               <MaterialCommunityIcons name='home' color={color} size={26} />
+             ),
+           }}
+         />
+         <Tab.Screen
+           name='Step Counter'
+           component={Counter}
+           options={{
+             tabBarLabel: 'Step Counter',
+             tabBarIcon: ({ color }) => (
+               <MaterialCommunityIcons name='watch' color={color} size={26} />
+             ),
+           }}
+         />
+         <Tab.Screen
+           name='Settings'
+           component={SettingsScreen}
+           options={{
+             tabBarLabel: 'Settings',
+             tabBarIcon: ({ color }) => (
+               <FontAwesome name='gear' color={color} size={26} />
+             ),
+           }}
+         />
+       </Tab.Navigator>
+     </NavigationContainer>
+   );
+         }else{
+          return(
+            <View>
+              <Login/>
+            </View>
+          )
+         }
+         
 }
 
-}
+
 const styles = StyleSheet.create({
   
 });
--- stedi-react-native-main/login.js
+++ stedi-react-native-main/login.js
@@ -1,46 +1,89 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { TouchableOpacity, SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
 
+const sendText = async (phoneNumber) => {
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin
+  await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/text"
+  }
+});
+  const LoninResponeText = await LoginResponse.text();
+  console.log('Login Response',loginResponseText);
+}
+
+const getToken = async({phoneNumber,otp}) =>{
+  const loginResponse=await fetch('https://dev.stedi.me/twofactorlogin/',{
+    method: 'POST',
+    headers:{
+      'content-type':'application/text'
+    },
+    body:{
+      phoneNumber,
+      oneTimePassword
+    }
+  });
+}
+  const token = await loginResponse.text();
+  console.log(token);
+  
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [otp, setOtp] = useState(null);
 
   return (
-    <SafeAreaView style={styles.mainView}>
+    <SafeAreaView style ={styles.mainView}>
       <TextInput
         style={styles.input}
         onChangeText={setPhoneNumber}
         value={phoneNumber}
-        placeholder="801-228-7536"
+        placeholder="801-555-1212"
       />
       <TextInput
         style={styles.input}
-        onChangeText={setOneTimePassword}
-        value={oneTimePassword}
+        onChangeText={setOtp}
+        value={otp}
         placeholder="1234"
         keyboardType="numeric"
-        secureTetEntry={true}
+        secureTextEntry={true}
       />
       <TouchableOpacity
         style={styles.button}
-        onPress={onPress}
+        onPress={()=>{sendText(phoneNumber)}}
       >
-        <Text>Press Here</Text>
+        <Text>Get OTP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Login</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 };
-
 
 const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
+    borderRadius: 5,
     padding: 10,
   },
   mainView:{
-    marginTop:100
+    marginTop: 100
+  },
+  button:{
+    alignItems:"center",
+    width: 100,
+    margin: 10,
+    borderwidth: 1,
+    borderRadius: 20, 
+    padding: 10,
+    backgroundColor: "#DDDD",
+    alignSelf: "center",
   }
 });
 
