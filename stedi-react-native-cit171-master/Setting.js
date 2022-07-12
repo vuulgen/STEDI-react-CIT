0666b75a9bcaa4a224d6efade7ce0d7bf1756489
Brendan Willis <vuulgen2@gmail.com>
2022-07-12 12:57:23 -0600

    project completion

--- stedi-react-native-cit171-master/Setting.js
+++ stedi-react-native-cit171-master/Setting.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Alert } from 'react-native';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
+ 
 
 function Setting() {
     return(
       <View>
-      <Button
-      title="Log In"
-      onPress={() => Alert.alert('Simple Button pressed')}
-    />
+     
       </View>
     )
   }
